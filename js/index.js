6ef1f2545e27d207f0ff1776eb5761f58c1e5ee5
Uriel Gartzia <ugartziasola@gmail.com>
2023-07-19 21:50:02 +0200

    iteracion 3 hecha

--- js/index.js
+++ js/index.js
@@ -17,7 +17,7 @@ const state = {
   greenPeppers: true,
   whiteSauce: false,
   glutenFreeCrust: false,
-  button: true,
+  button: true
 };
 
 // This function takes care of rendering the pizza based on the state
@@ -69,66 +69,87 @@ function renderWhiteSauce() {
   // Iteration 2: add/remove the class "sauce-white" of `<section class="sauce">`
   document.querySelectorAll('.sauce').forEach((oneSauce) => {
     if (!state.whiteSauce) {
-      oneSauce.classList.add("sauce-white");
+      oneSauce.classList.add('sauce-white');
     } else {
-      oneSauce.classList.remove("sauce-white");
+      oneSauce.classList.remove('sauce-white');
     }
   });
 }
-
 
 function renderGlutenFreeCrust() {
   // Iteration 2: add/remove the class "crust-gluten-free" of `<section class="crust">`
   document.querySelectorAll('.crust').forEach((oneCrust) => {
     if (!state.glutenFreeCrust) {
-      oneCrust.classList.add("crust-gluten-free");
+      oneCrust.classList.add('crust-gluten-free');
     } else {
-      oneCrust.classList.remove("crust-gluten-free");
+      oneCrust.classList.remove('crust-gluten-free');
     }
   });
 }
 
 function renderButtons() {
-// Iteration 3: add/remove the class "active" of each `<button class="btn">`
+  // Iteration 3: add/remove the class "active" of each `<button class="btn">`
 
-document.querySelectorAll(".btn").forEach((oneButton) => {
-  if (state.button) {
-    oneButton.classList.add("active");
-  } else {
-    oneButton.classList.remove("active");
-  }
-});
+ if (state.pepperoni) {
+  document.querySelector(".btn-pepperoni").classList.add("active")
+ } else {
+  document.querySelector(".btn-pepperoni").classList.remove("active")
+ }
 
+ if (state.mushrooms) {
+  document.querySelector(".btn-mushrooms").classList.add("active")
+ } else {
+  document.querySelector(".btn-mushrooms").classList.remove("active")
+ }
+
+ if (state.greenPeppers) {
+  document.querySelector(".btn-green-peppers").classList.add("active")
+ } else {
+  document.querySelector(".btn-green-peppers").classList.remove("active")
+ }
+
+ if (state.whiteSauce) {
+  document.querySelector(".btn-sauce").classList.add("active")
+ } else {
+  document.querySelector(".btn-sauce").classList.remove("active")
+ }
+
+ if (state.glutenFreeCrust) {
+  document.querySelector(".btn-crust").classList.add("active")
+ } else {
+  document.querySelector(".btn-crust").classList.remove("active")
+ }
 }
-
-  
- 
-  
 
 
 function renderPrice() {
   // Iteration 4: change the HTML of `<aside class="panel price">`
-
 }
 
 renderEverything();
 
 // Iteration 1: Example of a click event listener on `<button class="btn btn-pepperoni">`
-document.querySelector('.btn.btn-pepperoni').addEventListener('click', function () {
-  state.pepperoni = !state.pepperoni;
-  renderEverything();
-});
+document
+  .querySelector('.btn.btn-pepperoni')
+  .addEventListener('click', function () {
+    state.pepperoni = !state.pepperoni;
+    renderEverything();
+  });
 
 // Iteration 1: Add click event listener on `<button class="btn btn-mushrooms">`
-document.querySelector('.btn.btn-mushrooms').addEventListener('click', function () {
-  state.mushrooms = !state.mushrooms;
-  renderEverything();
-});
+document
+  .querySelector('.btn.btn-mushrooms')
+  .addEventListener('click', function () {
+    state.mushrooms = !state.mushrooms;
+    renderEverything();
+  });
 // Iteration 1: Add click event listener on `<button class="btn btn-green-peppers">`
-document.querySelector('.btn.btn-green-peppers').addEventListener('click', function () {
-  state.greenPeppers = !state.greenPeppers;
-  renderEverything();
-});
+document
+  .querySelector('.btn.btn-green-peppers')
+  .addEventListener('click', function () {
+    state.greenPeppers = !state.greenPeppers;
+    renderEverything();
+  });
 // Iteration 2: Add click event listener on `<button class="btn btn-sauce">`
 document.querySelector('.btn.btn-sauce').addEventListener('click', function () {
   state.whiteSauce = !state.whiteSauce;
